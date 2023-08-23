e567aea0cf7cbc8c9845bc91298c457f020d3587
MaykeBolt <mayke.silva6@etec.sp.gov.br>
2023-08-23 11:14:48 -0300

    Adicionado a base dos exemplos 04,05,06 e 07

---
+++ arraylist_generics/exemplo_java_poo_07/src/main/java/br/eteczl/App.java
@@ -0,0 +1,13 @@
+package br.eteczl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
