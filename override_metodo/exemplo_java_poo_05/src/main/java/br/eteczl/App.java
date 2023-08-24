fce0a06521f3d31c6b3586f30d2483047a1f307c
jaovg5 <jvgsantos05@gmail.com>
2023-08-24 08:56:13 -0300

    Override feito

--- override_metodo/exemplo_java_poo_05/src/main/java/br/eteczl/App.java
+++ override_metodo/exemplo_java_poo_05/src/main/java/br/eteczl/App.java
@@ -1,13 +1,21 @@
 package br.eteczl;
 
-/**
- * Hello world!
- *
- */
+import model.Veiculo;
+import model.carro;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       Veiculo v1= new Veiculo("modelo", 0);
+       carro car= new carro ("corsa", 30, 0);       
+       
+       v1.setVelocidade(0);
+       car.setVelocidade(40);
+       car.passmarc();
+       car.carbrand();
     }
+    
+    //É um conceito do polimorfismo que nos permite reescrever um método.
+    //Podemos reescrever nas classes filhas métodos criados inicialmente na classe pai, os métodos que serão sobreposto.
 }
