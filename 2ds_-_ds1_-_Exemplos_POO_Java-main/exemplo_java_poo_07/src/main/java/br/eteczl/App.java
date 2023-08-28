04b2cd5c131464ec390c2a9b6cb442ae8825546c
John Dias <120695464+JohnDias0@users.noreply.github.com>
2023-08-28 18:58:03 -0300

    Add files via upload
    commit final

---
+++ 2ds_-_ds1_-_Exemplos_POO_Java-main/exemplo_java_poo_07/src/main/java/br/eteczl/App.java
@@ -0,0 +1,20 @@
+package br.eteczl;
+import model.Animal;
+import model.Pessoa;
+
+public class App 
+{
+    Pessoa p = new Pessoa("Jonathan", 17);
+    p.andar();
+
+    Animal gato = new Animal('persa', 'feminino', 'gato')
+    gato.fazersom;
+
+    p.getNome;
+    p.setNome('João');
+
+    Pessoa j = new Pessoa("Mayke", 19);
+    p.getNome;
+    p.setNome("Luís");
+
+}
---
+++ 2ds_-_ds1_-_Exemplos_POO_Java-main/exemplo_java_poo_07/src/main/java/model/Cachorro.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Cachorro extends Animal {
+
+	public String nome;
+	
+	public Cachorro(String raca, String sexo, String tipo, String nome) {
+		super(raca,sexo,tipo);
+		this.nome = nome;
+		
+	}
+	//Método da class pai Animal sendo usado pelo Cachorro, mas com os Ajustes necessarios para ser um cachorro fazendo som
+	public void fazersom(){
+		//O Animal Cachorro também emiti som, mas com o seu ruído único.
+		System.out.println("AU AU");
+	}
+		
+}
